d0193392d37c1437ece5abed874ebc78a16d4018
Adedayo Adepoju <1mole.coco@gmail.com>
2023-01-05 23:46:11 +0100

    float-int type

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import math
+
+"""A function which takes a float as argument"""
+
+
+def floor(n: float) -> int:
+    """Returns the floor of the float"""
+    return math.floor(n)
