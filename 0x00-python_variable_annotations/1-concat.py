4592bbfe1480d3b2249e450589fe36e07af6bc8f
Adedayo Adepoju <1mole.coco@gmail.com>
2023-01-05 23:36:05 +0100

    str type

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
-'''
-    A function that takes two string arguments
-'''
+
+"""A function that takes two string arguments"""
+
 
 def concat(str1: str, str2: str) -> str:
-    '''
-        Return a concatenated string of the arguments
-    '''
+    """Return a concatenated string of the arguments"""
     return str1 + str2
