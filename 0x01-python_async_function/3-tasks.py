d3396563fd8379001363ddfa1921eb314d1d919e
Adedayo Adepoju <1mole.coco@gmail.com>
2023-01-09 20:46:01 +0100

    Initial commit

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""A normal function that takes and an integer max_delay"""
+
+import asyncio
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> str:
+    """The function returns a class asyncio.Task"""
+
+    response = asyncio.create_task(wait_random(max_delay))
+    return response
+
