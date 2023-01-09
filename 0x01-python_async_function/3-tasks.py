a7ad43a7d0bd41ce41ef44490a40da422f1c688d
Adedayo Adepoju <1mole.coco@gmail.com>
2023-01-09 23:08:00 +0100

    Initial commit

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -8,7 +8,7 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> str:
+def task_wait_random(max_delay: int) -> Task:
     """The function returns a class asyncio.Task"""
 
     response = asyncio.create_task(wait_random(max_delay))
